4b237c1466344a4362d390c493964cd48b4dc620
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2025-08-13 17:07:12 +0200

    WIP FELT: Introduce RemotePoliciesProvider to handle live policies
    Differential Revision: https://phabricator.services.mozilla.com/D261696

---
+++ toolkit/components/enterprisepolicies/tests/browser/browser_policies_basiclive_tests.js
@@ -0,0 +1,22 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// TODO: We only test the RemotePoliciesProvider here, it would be nice
+// to enhance with testing combinations of providers
+
+add_setup(async function test_set_http_server_usage() {
+  Services.prefs.setBoolPref("browser.policies.testUseHttp", true);
+  await EnterprisePolicyTesting.servePolicyWithJson(
+    {},
+    {},
+    registerCleanupFunction
+  );
+  assertOverHttp();
+});
+
+add_task(async function test_policy_cleanup() {
+  await EnterprisePolicyTesting.servePolicyWithJson({}, {});
+  assert_policy_cleanup();
+});
