b1850f2b84f9707c5a19f51343acfa045feb68ad
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2025-08-25 08:22:38 +0200

    FELT: Adding GitHub Actions handling for push and PR
    This is adding workflow to perform builds on Linux, Windows and macOS on
    push as well as pull requests events.
    
    It will triggers creation of instances in GCP to run the builds.
    
    Scripts are provided to also prepare the base disk image for both
    Windows and Linux.
    
    Build is done for all platforms on Linux hosts using cross-compilation.

---
+++ .github/workflows/ubuntu-install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+apt -y update && sudo apt -y upgrade
+apt -y install \
+        build-essential \
+        curl \
+        git \
+        libasound2-dev \
+        libdbus-glib-1-dev \
+        libgtk2.0-dev \
+        libgtk-3-dev \
+        libpython3-dev \
+        libx11-xcb-dev \
+        libxt-dev \
+        msitools \
+        make \
+        patch \
+        patchelf \
+        python3-dev \
+        python3-yaml \
+        python3-venv \
+        rsync \
+        squashfs-tools \
+        tar \
+        unzip \
+        uuid \
+        wget \
+        zip \
+        7zip
+
+export RUSTUP_HOME=/usr/local
+export CARGO_HOME=/usr/local 
+curl \
+	--proto '=https' --tlsv1.2 -sSf \
+	https://sh.rustup.rs | sh -s -- -y
+
+rustup target add x86_64-unknown-linux-gnu
+rustup target add x86_64-pc-windows-msvc
+rustup target add aarch64-apple-darwin
+rustup toolchain install stable 
+
+useradd worker -d /home/worker
+mkdir -p /home/worker
+chown worker:worker /home/worker
+
+sudo -u worker mkdir /home/worker/actions-runner
+sudo -u worker sh -c "cd /home/worker/actions-runner && curl -o actions-runner-linux-x64-2.328.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.328.0/actions-runner-linux-x64-2.328.0.tar.gz && tar xf actions-runner-linux-x64-2.328.0.tar.gz"
+
+poweroff
