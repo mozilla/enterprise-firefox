b31127c795145f583a9a76e7ffd365dc596663dd
Jonathan Mendez <jmendez@mozilla.com>
2026-02-20 23:17:31 -0600

    [DO NOT LAND] test repacks

--- taskcluster/gecko_taskgraph/target_tasks.py
+++ taskcluster/gecko_taskgraph/target_tasks.py
@@ -480,6 +480,9 @@ def target_tasks_enterprise_firefox_with_tests(
     )
 
     def filter(task):
+        if "shippable" in task.label and "enterprise" in task.label:
+            return True
+
         # Enabling tests suites triggers many jobs not required, limit execution
         # to enterprise builds for now
         if task.kind in ["test", "mochitest"] and "enterprise" not in task.label:
