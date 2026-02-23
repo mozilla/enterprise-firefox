5b444e3ae85fee36cb903404e2d6319ac2ecb680
Janika <jneuberger@mozilla.com>
2026-02-23 20:56:10 +0100

    Bug 2002562 - Test enterprise badge accessible by keyboard navigation

--- testing/enterprise/test_felt_browser_enterprise_toolbar_button.py
+++ testing/enterprise/test_felt_browser_enterprise_toolbar_button.py
@@ -10,6 +10,7 @@ sys.path.append(os.path.dirname(__file__))
 
 from base_test import Environment
 from felt_tests import FeltTests
+from marionette_driver.keys import Keys
 
 
 class EnterpriseBadgeTests(FeltTests):
@@ -18,6 +19,7 @@ class EnterpriseBadgeTests(FeltTests):
         self.connect_child_browser()
         self.assert_user_signed_in(env=Environment.FIREFOX)
         self.assert_enterprise_badge_and_panel()
+        self.assert_enterprise_panel_accessible_by_keypress()
 
     def assert_enterprise_badge_and_panel(self):
         user = self.get_logged_in_user_info(env=Environment.FIREFOX)
@@ -37,7 +39,7 @@ class EnterpriseBadgeTests(FeltTests):
         self._logger.info("Clicking enterprise panel")
         badge.click()
 
-        self._logger.info("Checking enterprise panel")
+        self._logger.info("Checking enterprise panel is displayed")
         self.get_elem_child("#panelUI-enterprise")
 
         self._logger.info("Checking user email address updated in enterprise panel")
@@ -45,5 +47,52 @@ class EnterpriseBadgeTests(FeltTests):
         assert email.get_property("textContent") == user["email"], (
             "User email not correctly set"
         )
+
+        self._logger.info("Closing enterprise panel")
+        chrome_body = self.get_elem_child("body")
+        chrome_body.send_keys(Keys.ESCAPE)
+
+        self._child_driver.set_context("content")
+
+    def assert_enterprise_panel_accessible_by_keypress(self):
+        self._child_driver.set_context("chrome")
+
+        # Move focus into chrome
+        chrome_body = self.get_elem_child("body")
+        chrome_body.send_keys(Keys.F6)
+
+        # Note: Sending keys on chrome body because the toolbar button
+        # is not reachable by keyboard (ElementNotInteractableException)
+        badge = self.get_elem_child("#enterprise-badge-toolbar-button")
+
+        def tab_and_get_next_active_element_id():
+            self._logger.info("Triggering Keys.TAB")
+            chrome_body.send_keys(Keys.TAB)
+            return self._child_driver.execute_script(
+                """
+                return document.activeElement;
+            """,
+                [],
+            )
+
+        self._logger.info("Tabbing through chrome elements until badge focused")
+        # This can cause an inifinite tab loop if the badge becomes unreachable
+        # in case of further changes to the toolbar. Worst case it results in a
+        # timeout and test failure.
+        self._wait.until(lambda _: tab_and_get_next_active_element_id() == badge)
+
+        is_focus_visible_on_badge = self._child_driver.execute_script(
+            """
+                return arguments[0].matches(":focus-visible");
+                """,
+            [badge],
+        )
+        assert is_focus_visible_on_badge, "Badge is not :focus-visible"
+
+        self._logger.info("Opening enterprise panel by keypress.")
+        chrome_body.send_keys(Keys.ENTER)
+
+        self._logger.info("Checking enterprise panel is displayed")
+        self.get_elem_child("#panelUI-enterprise")
 
         self._child_driver.set_context("content")
