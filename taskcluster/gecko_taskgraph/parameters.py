216d76f49cde374a7d8b2275f1ecf642b56e10aa
Andrew Halberstadt <ahal@mozilla.com>
2026-04-09 13:53:08 -0400

    fix: set release_type parameter on enteprise-firefox-try

--- taskcluster/gecko_taskgraph/parameters.py
+++ taskcluster/gecko_taskgraph/parameters.py
@@ -125,13 +125,7 @@ def get_app_version(product_dir="browser"):
 
 
 def get_release_type(parameters):
-    if parameters["project"] != "enterprise-firefox":
-        return ""
-
-    if (
-        not parameters["base_repository"]
-        == "https://github.com/mozilla/enterprise-firefox"
-    ):
+    if parameters["project"] not in ("enterprise-firefox", "enterprise-firefox-try"):
         return ""
 
     if parameters["head_ref"] == "refs/heads/enterprise-release":
